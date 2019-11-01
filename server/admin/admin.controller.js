eb8802709f9cd4fd9f5fb16e68a118b8e79f0558
letstrie <2016-314-436@student.cse.du.ac.bd>
2019-11-01 20:39:12 +0600

    minor changes

--- server/admin/admin.controller.js
+++ server/admin/admin.controller.js
@@ -1,9 +1,10 @@
 const ResearcherQuestionSetModel = require("../researcher/researcher.model");
+
 module.exports.allQuestionsSet = async (req, res) => {
-	try {
-		const questionList = await ResearcherQuestionSetModel.find({});
-		return res.json({ success: true, questionList });
-	} catch (error) {
-		next(error);
-	}
+  try {
+    const questionList = await ResearcherQuestionSetModel.find({});
+    return res.json({ success: true, questionList });
+  } catch (error) {
+    next(error);
+  }
 };

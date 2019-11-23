376ab64841005df03f6c2f48268c631ce3d7af2c
letstrie <2016-314-436@student.cse.du.ac.bd>
2019-11-23 18:01:01 +0600

    Account module demo frontend added (HTML && CSS)

--- config/logger.js
+++ config/logger.js
@@ -1,4 +1,4 @@
-const { createLogger, format, transports } = require('winston');
+const { createLogger, format } = require('winston');
 const winston = require('winston');
 
 module.exports = createLogger({
